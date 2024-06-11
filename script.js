bcc1a75fab1faeb8e96c43c233459cccdb0ddf2f
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-06-11 12:19:28 -0300

    ok

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     { //primeiro objeto da lista de perguntas
-        enunciado: "pergunta1", 
-        alternativas: ["alternativa1", "alternativa2"]
+        enunciado: "qual a melhor maneira de se combater o desmatamento?", 
+        alternativas: ["reflorestamento de áreas degradadas",
+                        "implementação de leis mais rigorosas para a proteção das florestas"]
     },
 
     { // segundo objeto da lista de pergunta
-        enunciado: "pergunta2", 
-        alternativas: ["alternativa1", "alternativa2"]
+        enunciado: "como podemos combater a desigualdade social de forma mais eficaz e sustentável?", 
+        alternativas: ["implementar programas de educação e formação profissional acessíveis a todos",
+                       "politícas de redistribuição de renda e oportunidades"]
     }
                 ];
+    let atual = 0;
+    let perguntaAtual;
+    
+  function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPergunta.textcontent =
+    
+  }
